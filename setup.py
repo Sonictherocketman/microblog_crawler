fcc3fd7a880135097d08524bc01640c4bdfddf8f
Brian Schrader <sonictherocketman@gmail.com>
2014-11-29 12:11:10 -0800

    fixed problems with PyPi upload

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+import os
+
+def read(fname):
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='MicroblogCrawler',
-    version='1.0',
+    name='microblogcrawler',
+    version='1.0.2',
     author='Brian Schrader',
     author_email='brian@biteofanapple.com',
     packages=['microblogcrawler', 'test'],
@@ -10,7 +14,7 @@ setup(
     url='https://github.com/Sonictherocketman/microblog_feedcrawler',
     license='LICENSE.txt',
     description='A basic microblog/rss feed crawler modeled after the Tweepy StreamListener.',
-    long_description=open('README.md').read(),
+    long_description=read('README.md'),
     install_requires=[
         'datetime',
         'dateutil',
