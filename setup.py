29c27ecfa2e0d5865a7068bb119503c8e2b20d9c
Brian Schrader <sonictherocketman@gmail.com>
2014-12-29 16:11:24 -0800

    Release v. 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='MicroblogCrawler',
-    version='1.0.31',
+    version='1.1',
     author='Brian Schrader',
     author_email='brian@biteofanapple.com',
     packages=['microblogcrawler', 'test'],
